3a9e493bb80e20b811facee975cc2f9287ffa2a1
Amos Benari <amos.benari@redhat.com>
2011-01-23 14:06:00 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Search::Application.config.secret_token = '228de50e726466ccdf61fab32727c522ac6ed010c76557653844b8d87205a1cfe6994d6e57336563b40fd8ab8dfebe8b479f2defd568f7e078fdecf2f1ff5853'
